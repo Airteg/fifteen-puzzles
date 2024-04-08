7b14047a4dfe21b3a9558f5f0056cc0d88869773
Airteg <airteg@ukr.net>
2024-04-08 13:10:18 +0300

    2. smile: fully ready

--- src/components/elements/canvas/smile.js
+++ src/components/elements/canvas/smile.js
@@ -19,76 +19,86 @@ import { wwN } from "../../../global/global-stiles.js";
 
 export default function Smile({ cx = 10, cy = 10, scale = 1 }) {
   return (
-    <Group transform={[{ scale: 2 }, { translateX: cx }, { translateY: cy }]}>
+    <Group transform={[{ scale }, { translateX: cx }, { translateY: cy }]}>
       {/* ------ Овал обличчя ------ */}
       <Path
-        color="#000"
-        style="stroke"
+        color="#FAFF3F"
+        style="fill"
         path="M 0 44 A 44 44 0 1 0 88 44 A 44 44 0 1 0 0 44 "
-      ></Path>
+      >
+        <Shadow dx={0} dy={4} blur={4} color="#00000019" />
+      </Path>
       <Path
-        color="#000"
-        style="stroke"
+        color="#FAFF3F"
+        style="fill"
         path="M 1.5 44 A 42.5 42.5 0 1 0 86.5 44 A 42.5 42.5 0 1 0 1.5 44 "
-      ></Path>
-
+      >
+        <Shadow dx={0} dy={0} blur={5} color="#80909290" inner />
+      </Path>
+      {/* Посмішка */}
       <Path
         color="#000"
         style="stroke"
+        strokeWidth={2}
+        strokeCap="round"
         path="M 49.7 69.5 C 41.7 72.3 24.9 73.1 21.8 54.1 "
       ></Path>
-
+      {/* Ліва брова */}
       <Path
         color="#000"
         style="stroke"
+        strokeWidth={2}
+        strokeCap="round"
         path="M 20.4 20.9 C 23.3 15.7 33.1 11.5 39.8 15.9 "
       ></Path>
+      {/* Права брова */}
       <Path
         color="#000"
         style="stroke"
+        strokeWidth={2}
+        strokeCap="round"
         path="M 66.3 29.6 C 73.1 31.1 77.5 38.4 76.3 45.1 "
       ></Path>
+      {/* Ліве око */}
       <Path
         color="#000"
         style="stroke"
-        path="M 54.7 45.4 A 7.3 7.3 0 1 0 69.4 45.4 A 7.3 7.3 0 1 0 54.7 45.4 "
-      ></Path>
-      <Path
-        color="#000"
-        style="stroke"
-        path="M 55.2 45.4 A 6.8 6.8 0 1 0 68.9 45.4 A 6.8 6.8 0 1 0 55.2 45.4 "
-      ></Path>
-      <Path
-        color="#000"
-        style="stroke"
+        strokeWidth={1}
         path="M 26.3 32 A 7.3 7.3 0 1 0 41 32 A 7.3 7.3 0 1 0 26.3 32 "
-      ></Path>
+      />
       <Path
-        color="#000"
-        style="stroke"
+        color="#fff"
+        style="fill"
         path="M 26.8 32 A 6.8 6.8 0 1 0 40.5 32 A 6.8 6.8 0 1 0 26.8 32 "
-      ></Path>
+      >
+        <Shadow dx={4} dy={4} blur={4} color="#71D4EBd0" inner />
+      </Path>
       <Path
         color="#000"
-        style="stroke"
+        style="fill"
         path="M 30.7 31.1 A 3.4 3.4 0 1 0 37.5 31.1 A 3.4 3.4 0 1 0 30.7 31.1 "
       ></Path>
-
+      {/* Праве око */}
       <Path
         color="#000"
         style="stroke"
+        strokeWidth={1}
+        path="M 54.7 45.4 A 7.3 7.3 0 1 0 69.4 45.4 A 7.3 7.3 0 1 0 54.7 45.4 "
+      />
+      <Path
+        color="#fff"
+        style="fill"
+        path="M 55.2 45.4 A 6.8 6.8 0 1 0 68.9 45.4 A 6.8 6.8 0 1 0 55.2 45.4 "
+      >
+        <Shadow dx={4} dy={4} blur={4} color="#71D4EBd0" inner />
+      </Path>
+      <Path
+        color="#000"
+        style="fill"
         path="M 58.9 44.8 A 3.4 3.4 0 1 0 65.7 44.8 A 3.4 3.4 0 1 0 58.9 44.8"
       />
 
       {/* ------ Елементи обличчя ------ */}
-
-      {/* Праве око */}
-
-      {/* Ліва брова */}
-
-      {/* Права брова */}
-
-      {/* Посмішка */}
     </Group>
   );
 }
