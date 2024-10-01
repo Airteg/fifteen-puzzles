0ae3c8577215d4a147a0dda969fbb9afbb4ea463
Airteg <airteg@ukr.net>
2024-10-01 09:35:38 +0300

    Created new Logo on Skia

--- src/app/Edit.jsx
+++ src/app/Edit.jsx
@@ -1,33 +1,18 @@
 import { View, Image } from "react-native";
 // import { Image } from "expo-image";
 import React, { useEffect, useRef, useState } from "react";
-import styled from "@emotion/native";
+import styled, { css } from "@emotion/native";
 import { Audio } from "expo-av";
 import { dfjccaic, hw } from "../global/global-stiles.js";
 import moveSound from "../assets/sound/move.aac";
 
 import SvgLogo from "../components/svg/Logo.jsx";
-import {
-  Circle,
-  ClipPath,
-  Defs,
-  ForeignObject,
-  G,
-  LinearGradient,
-  RadialGradient,
-  Filter,
-  FeColorMatrix,
-  Ellipse,
-  Polygon,
-  Text,
-  Mask,
-  Rect,
-  Stop,
-  Svg,
-  SvgXml,
-  Use,
-} from "react-native-svg";
-import Test from "../components/svg/Test";
+
+import Test from "../components/svg/TestCanvase";
+import Logo from "../components/elements/canvas/Logo.jsx";
+import Button from "../components/elements/jsx/Button.jsx";
+import { Back, BackActive } from "../assets/index.js";
+import { router } from "expo-router";
 
 const Edit = () => {
   const soundRef = useRef(null);
@@ -52,13 +37,23 @@ const Edit = () => {
       await soundRef.current.replayAsync();
     }
   };
-
+  const size = 110;
   return (
     <Container>
-      {/* <Test /> */}
-      <TestContainer>
-        <SvgLogo />
-      </TestContainer>
+      <Part1>
+        <TestContainer size={size}>
+          <Logo size={size} />
+        </TestContainer>
+      </Part1>
+      <Part2>
+        <View style={containerStyle.smallButton}>
+          <Button
+            onPress={() => router.back()}
+            backgroundImage={Back}
+            activeBackgroundImage={BackActive}
+          />
+        </View>
+      </Part2>
     </Container>
   );
 };
@@ -67,12 +62,45 @@ export default Edit;
 
 const Container = styled(View)`
   flex: 1;
-  border: 1px solid darkgreen;
+  border: 0.5px solid darkgreen;
   ${dfjccaic}
 `;
 const TestContainer = styled(View)`
-  border: 1px solid red;
-  width: 250px;
-  height: 250px;
+  /* border: 0.5px solid red; */
+  width: ${(props) => {
+    return props.size.toString();
+  }}px;
+  height: ${(props) => props.size.toString()}px;
   overflow: visible;
 `;
+const Part1 = styled(View)`
+  flex: 0.8;
+  justify-content: center;
+`;
+const Part2 = styled(View)`
+  flex: 0.2;
+  justify-content: flex-end;
+  align-self: flex-end;
+`;
+const containerStyle = {
+  image: css`
+    flex: 1;
+    justify-content: center;
+    width: 100%;
+  `,
+  bigButton: css`
+    align-self: flex-end;
+    width: 100%;
+    height: ${hw(74)}px;
+    /* border: solid 1px blue; */
+    ${dfjccaic}
+  `,
+  smallButton: css`
+    width: ${hw(64)}px;
+    height: ${hw(64)}px;
+    display: flex;
+    justify-content: center;
+    align-self: flex-end;
+    /* border: solid 1px blue; */
+  `,
+};
--- src/components/svg/Logo.jsx
+++ src/components/svg/Logo.jsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { View } from "react-native";
+import { SkiaShadow } from "react-native-skia-shadow";
 import Svg, {
   G,
   Path,
@@ -9,15 +11,15 @@ import Svg, {
   Use,
 } from "react-native-svg";
 
-/* SVGR has dropped some elements not supported by react-native-svg: filter */
 const SvgLogo = (props) => (
-  <Svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="-3 -3 106 106"
-    fill="none"
-    {...props}
-  >
-    <G id="back">
+  <SkiaShadow blur={10} dx={0} dy={0} borderRadius={18} color="#00000077">
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 100 100"
+      fill="none"
+      {...props}
+    >
+      {/* <G id="back">
       <Path
         d="M 0 10 Q 0.07 5.811 2.937 2.925 L 10 10 V 90 L 2.929 97.071 Q 0.042 94.104 0.007 89.987 Z"
         // fill="none"
@@ -30,34 +32,45 @@ const SvgLogo = (props) => (
     </G>
     <Use href="#back" rotation={90} origin="50, 50" />
     <Use href="#back" rotation={180} origin="50, 50" />
-    <Use href="#back" rotation={270} origin="50, 50" />
-    <Path
+    <Use href="#back" rotation={270} origin="50, 50" /> */}
+      {/* <Path
       d="M 0 10 C 0 4.5 4.5 0 10 0 H 90 C 96.5 0 100 4.5 100 10 L 100 90 C 100 96.5 96.5 100 90 100 H 10 C 4.5 100 0 96.5 0 90 Z"
       fill="#D5F7FFe2"
       // stroke="#D5F7FF"
       stroke="lightblue"
       strokeWidth={0}
-    />
-    <G origin="50, 50" scale={0.92}>
-      <Path
-        d="M 0 10 C 0 3 3 0 10 0 H 90 C 97 0 100 3 100 10 L 100 90 C 100 97 97 100 90 100 H 10 C 3 100 0 97 0 90 Z"
-        fill="none"
-        stroke="#D5F7FF"
-        // stroke="red"
-        strokeWidth={2}
-      />
-    </G>
+    /> */}
 
-    <Defs>
-      <LinearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%">
-        <Stop offset="0%" stopColor="#fff" stopOpacity={1} />
-        <Stop offset="45%" stopColor="#000" stopOpacity={1} />
-        <Stop offset="45%" stopColor="#000" stopOpacity={1} />
-        <Stop offset="55%" stopColor="#000" stopOpacity={1} />
-        <Stop offset="55%" stopColor="#000" stopOpacity={1} />
-        <Stop offset="100%" stopColor="#fff" stopOpacity={1} />
-      </LinearGradient>
-    </Defs>
-  </Svg>
+      <G origin="50, 50" scale={0.92}>
+        <Path
+          d="M 0 10 C 0 3 3 0 10 0 H 90 C 97 0 100 3 100 10 L 100 90 C 100 97 97 100 90 100 H 10 C 3 100 0 97 0 90 Z"
+          fill="none"
+          stroke="#D5F7FF"
+          // stroke="red"
+          strokeWidth={2}
+        />
+      </G>
+      <G origin="50, 50" scale={0.85}>
+        <Path
+          d="M 0 10 C 0 3 3 0 10 0 H 90 C 97 0 100 3 100 10 L 100 90 C 100 97 97 100 90 100 H 10 C 3 100 0 97 0 90 Z"
+          fill="#D5F7FF"
+          stroke="#D5F7FF"
+          // stroke="red"
+          strokeWidth={0}
+        />
+      </G>
+      <Defs>
+        <LinearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%">
+          <Stop offset="0%" stopColor="#fff" stopOpacity={1} />
+          <Stop offset="45%" stopColor="#000" stopOpacity={1} />
+          <Stop offset="45%" stopColor="#000" stopOpacity={1} />
+          <Stop offset="55%" stopColor="#000" stopOpacity={1} />
+          <Stop offset="55%" stopColor="#000" stopOpacity={1} />
+          <Stop offset="100%" stopColor="#fff" stopOpacity={1} />
+        </LinearGradient>
+      </Defs>
+    </Svg>
+    <View />
+  </SkiaShadow>
 );
 export default SvgLogo;
